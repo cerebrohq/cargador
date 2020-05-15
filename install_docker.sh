caf3760c274b7bf8a08207f7fe619ba537577e59
cerebroSupport <41910371+cerebroSupport@users.noreply.github.com>
2020-05-15 13:11:37 +0300

    Add files via upload

---
+++ install_docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+while getopts ":u:" opt; do
+	case $opt in
+		u ) parameterU="$OPTARG";;
+	esac
+done
+
+mkdir -p ./logs
+
+echo "Installing docker..."
+
+if !(command_exists docker) || [[ "$parameterU" == "true" ]] ; then
+	if !(command_exists curl); then
+		echo "curl command not found, please install curl"
+		exit 2
+	fi
+
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	sh get-docker.sh > ./logs/docker_install.log
+
+	echo "Docker installed successfully"
+else
+	echo "Docker already installed, update skipped. If you Want to update docker use -u true parameter"
+fi
