4f1aa0c61ba08eef335c43050422a9668385e45a
cerebroSupport <41910371+cerebroSupport@users.noreply.github.com>
2020-10-14 20:07:36 +0300

    Add files via upload

--- restoreDB.sh
+++ restoreDB.sh
@@ -1,8 +1,11 @@
 docker exec cargador /bin/sh -c "/cerebro/cargador/restoreDB.sh $*"
 
+if [ -e ./config/.system/catalog.sqlite.new ]
+then
 ./stop_cargador.sh
 
 rm ./config/.system/catalog.sqlite
 mv ./config/.system/catalog.sqlite.new ./config/.system/catalog.sqlite
 
 ./start_cargador.sh
+fi
